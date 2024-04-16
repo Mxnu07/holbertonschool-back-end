e8ae8409cee77273cbafd6293d4c8457bac6b34d
MMD <manuelmoralesdiaz0@gmail.com>
2024-04-16 19:09:50 -0400

    changing code

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,53 +1,27 @@
 #!/usr/bin/python3
-"""Retrieves and exports an employee's TODO list progress in JSON format"""
 import json
 import requests
 import sys
 
+employee_id = sys.argv[1]
 
-def get_employee_todos(employee_id):
-    """Retrieves the employee's todos from the API"""
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    response = requests.get(url)
-    todos = response.json()
-    return todos
+employee_data = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                             employee_id)
+employee_data_json = employee_data.json()
 
+employee_username = employee_data_json['username']
 
-def get_employee_name(employee_id):
-    """Retrieves the employee's name from the API"""
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(url)
-    employee = response.json()
-    return f"{employee.get('name')}"
+todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
+                         employee_id)
+todo_data_json = todo_data.json()
 
+total_todos = str(len(todo_data_json))
+completed_todos = str(sum(1 for task in todo_data_json if task['completed']))
 
-def export_todo_progress(employee_id):
-    """Exports the employee's TODO list progress in JSON format"""
-    todos = get_employee_todos(employee_id)
-    employee_name = get_employee_name(employee_id)
-    todo_data = []
-
-    for task in todos:
-        task_dict = {
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": employee_name,
-        }
-        todo_data.append(task_dict)
-
-    filename = f"{employee_id}.json"
-    todo_json = {str(employee_id): todo_data}
-
-    with open(filename, mode="w") as jsonfile:
-        json.dump(todo_json, jsonfile)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            export_todo_progress(employee_id)
-        except ValueError:
-            print("Employee ID must be an integer")
+with open(f'{employee_id}.json', 'w') as jsonfile:
+    json.dump({employee_id: [
+        {
+            "task": task['title'],
+            "completed": task['completed'],
+            "username": employee_username
+        } for task in todo_data_json]}, jsonfile)
